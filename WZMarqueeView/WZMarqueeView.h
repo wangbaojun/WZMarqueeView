138dadf8e6d1f0fc272bc1e7863ab3bc88fc38db
netease <bjwangbaojun@corp.netease.com>
2016-04-19 19:51:38 +0800

    release 1.0.0

---
+++ WZMarqueeView/WZMarqueeView.h
@@ -0,0 +1,13 @@
+//
+//  WZMarqueeView.h
+//  BZLib
+//
+//  Created by netease on 16/4/19.
+//  Copyright © 2016年 netease. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WZMarqueeView : NSObject
+
+@end
---
+++ WZMarqueeView/WZMarqueeView.m
@@ -0,0 +1,13 @@
+//
+//  WZMarqueeView.m
+//  BZLib
+//
+//  Created by netease on 16/4/19.
+//  Copyright © 2016年 netease. All rights reserved.
+//
+
+#import "WZMarqueeView.h"
+
+@implementation WZMarqueeView
+
+@end
